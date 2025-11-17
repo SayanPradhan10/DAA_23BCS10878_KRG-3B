c7d876bbd6f6d2c40608f7bf7504823766a2d40d
SayanPradhan10 <sayan.2023.300@gmail.com>
2025-11-17 12:27:10 +0530

    Refactor platform calculation logic

--- End Sem Practicals/GFG Minimum Platform.cpp
+++ End Sem Practicals/GFG Minimum Platform.cpp
@@ -7,25 +7,22 @@ class Solution {
         sort(arr.begin(), arr.end());
         sort(dep.begin(), dep.end());
         
-        int platforms = 1;     // current platforms needed
-        int maxPlatforms = 1;  // result
+        int platforms = 1;    
+        int maxPlatforms = 1;  
         int i = 1, j = 0;      
         
-        // Step 2: Two pointer comparison
+    
         while (i < n && j < n) {
             
-      // If train arrives before previous one departs, add a platform
             if (arr[i] <= dep[j]) {
                 platforms++;
                 i++;
             }
-            // One train departed  free a platform
             else {
                 platforms--;
                 j++;
             }
-            
-            // Update max required platforms
+
             maxPlatforms = max(maxPlatforms, platforms);
         }
         
